7e4476630b9093be1a7f432a5a71d9f240919641
CARLOS_LAP\cone_ <carloscastillodeveloper@gmail.com>
2025-02-05 16:52:25 -0600

    commit inicial

---
+++ src/main/java/com/example/medicChest/MCH_ApiGateway/MchApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicChest.MCH_ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MchApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MchApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medicChest/MCH_ApiGateway/MchApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicChest.MCH_ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MchApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
